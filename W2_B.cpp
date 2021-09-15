d160c798df90ee0638138e850bc8c73f3242209d
JaryoGuzoKiller <73612660+JaryoGuzoKiller@users.noreply.github.com>
2021-09-15 18:47:36 +0900

    Update W2_B.cpp

--- W2_B.cpp
+++ W2_B.cpp
@@ -1,7 +1,11 @@
 #include<iostream>
-#include<vector>
+#include<utility>
+#include<algorithm>
 using namespace std;
 
+bool compare(const pair<int, int>& a, const pair<int, int>& b) {
+	return a.first + a.second > b.first + b.second;
+}
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
@@ -9,54 +13,35 @@ int main() {
 
 	int T;
 	cin >> T;
+
+
 	while (T--) {
 		int N;
 		cin >> N;
 
-		vector<int> a;
-		vector<int> b;
+		pair<int, int>* src = new pair<int, int>[N];
 
+		int a_src, b_src;
 		for (int i = 0; i < N; i++) {
-			int num1, num2;
-			cin >> num1 >> num2;
-
-			a.push_back(num1);
-			b.push_back(num2);
+			cin >> a_src >> b_src;
+			src[i].first = a_src;
+			src[i].second = b_src;
 		}
+		sort(&src[0], &src[N], compare); //두 자원의 합으로 내림차순 정렬
 
-		int diff = 0, temp = 0;
-		int a_result = 0, b_result = 0;
-		bool flag = true;
-		int max = a[0] - b[0];
-		while (!a.empty()) {
-			for (int i = 0; i < a.size(); i++) {
+		int inha_src = 0, dragon_src = 0;
 
-				if (flag == true)
-					diff = a[i] - b[i];
-				else {
-					diff = (a[i] - b[i]) * -1;
-					max = max * -1;
-				}
-				if (max <= diff) {
-					temp = i;
-					max = diff;
-				}
+		int flag = 0;
+		for (int i = 0; i < N; i++) {
+			if (flag == 0) {
+				inha_src += src[i].first;
+				flag = 1;
 			}
-			max = a[0] - b[0];
-			if (flag == true) {
-				a_result = a_result + a[temp];
-				a.erase(a.begin() + temp);
-				b.erase(b.begin() + temp);
-				flag = false;
+			else if (flag == 1) {
+				dragon_src += src[i].second;
+				flag = 0;
 			}
-			else if (flag == false) {
-				b_result = b_result + b[temp];
-				a.erase(a.begin() + temp);
-				b.erase(b.begin() + temp);
-				flag = true;
-			}
-			
 		}
-		cout << a_result - b_result << "\n";
+		cout << inha_src - dragon_src << "\n";
 	}
 }
