1536a90601bff29af3862cdbb584daa289c16dd0
JaryoGuzoKiller <73612660+JaryoGuzoKiller@users.noreply.github.com>
2021-10-22 04:04:42 +0900

    Update W1_B.cpp

--- W1_B.cpp
+++ W1_B.cpp
@@ -1,19 +1,7 @@
 #include<iostream>
 #include<vector>
+#include<stack>
 using namespace std;
-
-void print(vector<int> list) {
-	int max = 1;
-	int min = 1000;
-	for (int i = 0; i < list.size(); i++) {
-		if (max < list[i])
-			max = list[i];
-		if (min > list[i])
-			min = list[i];
-	}
-
-	cout << min << " " << max << "\n";
-}
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -27,7 +15,9 @@ int main() {
 		int N;
 		cin >> N;
 
-		vector<int> tower;
+		stack<int> tower;
+		stack<int> min;
+		stack<int> max;
 		while (N--) {
 			int q;
 			cin >> q;
@@ -35,14 +25,29 @@ int main() {
 			if (q == 1) {
 				int num;
 				cin >> num;
+				tower.push(num);
 
-				tower.push_back(num);
+				if (min.empty())
+					min.push(num);
+				else if(min.top() >= num)
+					min.push(num);
 
-				print(tower);
+				if (max.empty())
+					max.push(num);
+				else if(max.top() <= num)
+					max.push(num);
+
+				cout << min.top() << " " << max.top() << "\n";
 			}
 			else if (q == 2) {
-				if(!tower.empty())
-					tower.pop_back();
+				if (!tower.empty()) {
+					if (min.top() == tower.top())
+						min.pop();
+					if (max.top() == tower.top())
+						max.pop();
+
+					tower.pop();			
+				}
 			}			
 		}
 	}
