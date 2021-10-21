cb484927b469b9d3c053f78fbaebea1f9c8ec77c
JaryoGuzoKiller <73612660+JaryoGuzoKiller@users.noreply.github.com>
2021-10-22 04:03:53 +0900

    Update W1_A.cpp

--- W1_A.cpp
+++ W1_A.cpp
@@ -1,78 +1,41 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+int T;
+string s;
+int k;
+vector<char>* arr = { NULL, };
+
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+	cin >> T;
+	while (T--) {
+		cin >> s;
+		cin >> k;
+		arr = new vector<char>[k+1];
 
-	int n;
-	cin >> n;
+		
+		int garo;
+		if (s.size() % k != 0)
+			garo = s.size() / k + 1;
+		else
+			garo = s.size() / k;
 
-	vector<char*> zigList;
-	while (n--) {
-		int row;
-		string word;
-		cin >> word;
-		cin >> row;
-
-
-		word.c_str();
-
-		int numOfzig = word.size() / row;
-		if (word.size() % row != 0)
-			numOfzig++;
-
-		char** zig = new char* [row + 1];
-		for (int i = 0; i < row + 1; ++i) {
-			zig[i] = new char[numOfzig + 1];
-			
+		for (int i = 1; i <= k; i++) {
+			for (int j = 1; j <= garo; j++) {
+				if (j == 1&& (j * i - 1) < s.size())
+					arr[i].push_back(s[j * i - 1]);
+				else if (j % 2 == 0&&(j * k - i)<s.size())
+					arr[i].push_back(s[j * k - i]);
+				else if(j % 2 != 0&& ((j - 1) * k + i - 1)<s.size())
+					arr[i].push_back(s[(j - 1) * k + i - 1]);
+			}
 		}
-
-		for (int i = 0; i < row; i++) {                       //확인위해 0부터 차례로 값넣어줍니다.
-
-			for (int j = 0; j < numOfzig + 1; j++) {
-
-				zig[i][j] = NULL;
-
-			}
-
-		}
-		int i = 0, j = 1;
-		int flag = 1;
-		for (int m = 0; m < word.size(); m++) {
-
-			if (m % row == 0) {
-
-				if (m != 0 && flag % 2 == 1) {
-					j++;
-					i++;
-					flag++;
-				}
-				else if (m != 0 && flag % 2 == 0) {
-					i--;
-					j++;
-					flag++;
-				}
-			}
-
-			if (flag % 2 == 1) {
-				i++;
-			}
-			else {
-				i--;
-			}
-			zig[i][j] = word[m];
-		}
-
-		for (int i = 1; i < row+1; i++) {
-			for (int j = 1; j < numOfzig+1; j++) {
-				if (zig[i][j] != NULL) {
-					cout << zig[i][j];
-				}
+		for (int i = 1; i <= k; i++) {
+			for (int j = 0; j < arr[i].size(); j++) {
+				if(arr[i][j]!=NULL)
+					cout << arr[i][j];
 			}
 		}
 		cout << "\n";
 	}
-	//
 }
