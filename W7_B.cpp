44cff29def3e0533eb0b0e4608416b05ea8261a2
JaryoGuzoKiller <73612660+JaryoGuzoKiller@users.noreply.github.com>
2021-12-16 02:46:02 +0900

    Update W7_B.cpp

--- W7_B.cpp
+++ W7_B.cpp
@@ -1,64 +1,43 @@
-//
-// Created by °­¼Ò¸Á on 2021/10/15.
-//
-#include <iostream>
-#include <vector>
+#include<iostream>
+#include<queue>
 #include<cmath>
-#include <algorithm>
-#include <queue>
 using namespace std;
-struct Info {
-    double dist;
-    int x;
-    int y;
-    Info(double dist, int x, int y) {
-        this->dist = dist;
-        this->x = x;
-        this->y = y;
-    }
-    bool operator<(const Info a)const {
-        if (this->dist == a.dist) {
-            if (this->x == a.x) {
-                return a.y > this->y;
-            }
-            return a.x > this->x;
-        }
-        return a.dist > this->dist;
-    }
-};/*
-struct cmp{
-    bool operator()(Info a,Info b){
-        if(a.dist == b.dist){
-            if(a.x==b.x){
-                return a.y > b.y;
-            }
-            return a.x > b.x;
-        }
-        return a.dist>b.dist;
-    }
-};*/
+int T, M, N, K, x, y;
+struct point {
+	int x, y;
+	double dist;
+	point(int x, int y, double dist) {
+		this->x = x;
+		this->y = y;
+		this->dist = dist;
+	}
+	bool operator<(const point a)const {
+		if (this->dist == a.dist) {
+			if (this->x == a.x) {
+				return a.y > this->y;
+			}
+			return a.x > this->x;
+		}
+		return a.dist > this->dist;
+	}
+};
 int main() {
-    int tc;
-    cin >> tc;
-    while (tc--) {
-        int m, n, k;
-        cin >> m >> n >> k;
-        priority_queue<Info> stores;
-        for (int i = 0; i < m; i++) {
-            int x, y;
-            cin >> x >> y;
-            double dist = sqrt(x * x + y * y);
-            stores.push(Info(dist, x, y));
-            if (stores.size() > k) stores.pop();
-        }
-        for (int i = 0; i < n; i++) {
-            int x, y;
-            cin >> x >> y;
-            double dist = sqrt(x * x + y * y);
-            stores.push(Info(dist, x, y));
-            stores.pop();
-            cout << stores.top().x << " " << stores.top().y << endl;
-        }
+	cin >> T;
+	while (T--) {
+		cin >> M >> N >> K;
+		priority_queue<point> stores;
 
-    }
+		for (int i = 1; i <= M; i++) {
+			cin >> x >> y;
+			stores.push(point(x, y, sqrt(x * x + y * y)));
+			if (stores.size() > K)
+				stores.pop();
+		}
+		for (int i = 1; i<= N; i++) {
+			cin >> x >> y;
+			stores.push(point(x, y, sqrt(x * x + y * y)));
+			stores.pop();
+			cout << stores.top().x << " " << stores.top().y << "\n";
+		}
+	}
 }
